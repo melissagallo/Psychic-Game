d9f36db192d394092bb119f3c78df6e4015c076b
Melissa gallo <melissa@melissagallopaints.com>
2018-03-08 14:43:14 -0500

    working on the js file

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,1 +1,51 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "x", "y", "z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "x", "y", "z"];
+newGame = location.reload();
+compRandomLetter = generateLetter();
+wins = 0;
+losses = 0;
+guessesLeft;
+userletterChoice = alphabet[];
+
+
+//First determine if user has started a new game. Set the Guesses to 9 and WINS and LOSSES to 0.  Then have the computer generate a random letter.
+
+
+if (newGame === true) {
+    guessesleft = 9;
+    wins = 0;
+    losses = 0;
+    generateLetter();
+}
+
+
+//Have player type in a letter.
+if (userLetterChoice !== compRandomLetter) {
+    losses = (i++),
+    guessesLeft = (i--),
+    document.getElementById('losses'),
+    textContent = "losses";
+}
+
+else if(userLetterChoice === compRandomLetter) {
+    wins = i++,
+    guessesLeft = i--;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generateLetter() {
+    var chars = "abcdefghijklmnopqurstuvwxyz";
+    return chars.substr(Math.floor(Math.random() * 26), 1);
+} 
