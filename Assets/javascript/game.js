4e22c35fd095480d05f8654b0230278915d508d8
Melissa gallo <melissa@melissagallopaints.com>
2018-03-09 13:27:00 -0500

    finalizing javascript

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,37 +1,55 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "x", "y", "z"];
-newGame = location.reload();
-compRandomLetter = generateLetter();
-wins = 0;
-losses = 0;
-guessesLeft;
-userletterChoice = alphabet[];
-
+// var newGame = location.reload();
+var compRandomLetter;
+var wins = 0;
+var losses = 0;
+var guessesLeft;
+var elGuesses = document.getElementById('guesses');
+var elWins = document.getElementById('wins');
+var elLosses = document.getElementById('losses');
 
 //First determine if user has started a new game. Set the Guesses to 9 and WINS and LOSSES to 0.  Then have the computer generate a random letter.
 
 
-if (newGame === true) {
-    guessesleft = 9;
-    wins = 0;
-    losses = 0;
-    generateLetter();
-}
+// if (newGame === true) {
+//     guessesleft = 9,
+//         wins = 0,
+//         losses = 0,
+//         generateLetter();
+// }
 
 
-//Have player type in a letter.
-if (userLetterChoice !== compRandomLetter) {
-    losses = (i++),
-    guessesLeft = (i--),
-    document.getElementById('losses'),
-    textContent = "losses";
-}
+//Have player type in a letter. Then determine if it is equal to the computer's choice or not.  If it is then WINS goes up by 1 and the guesses return to O.  If the player's choice is not equal to that of the computer than the losses go up by 1 and the guesses left go down by 1.
 
-else if(userLetterChoice === compRandomLetter) {
-    wins = i++,
-    guessesLeft = i--;
+document.onkeyup = function (event) {
+
+    var LetterChoice = event.key;
+    console.log(LetterChoice);
+    if (LetterChoice === compRandomLetter) {
+        wins++;
+        console.log("you won");
+        elWins.innerHTML = wins;
+        newRound();
+    }
+      else{
+           
+            guessesLeft--;
+            if(guessesleft === 0){
+                losses++;
+                newRound();
+            }
+            
+            elLosses.innerHTML = losses;
+
+        }
+    }
 
 
-}
+    // continue the game until user runs out of guesses (9), or gets a win.  The player can keep going to get as many wins as he or she wants.
+
+
+
+
 
 
 
@@ -46,6 +64,15 @@ else if(userLetterChoice === compRandomLetter) {
 
 
 function generateLetter() {
-    var chars = "abcdefghijklmnopqurstuvwxyz";
-    return chars.substr(Math.floor(Math.random() * 26), 1);
-} 
+
+    var randomIndex = Math.floor(Math.random() * 26);
+    compRandomLetter = alphabet[randomIndex];
+
+}
+
+function newRound() {
+    //Here you want to reset all of the values in your game to default.
+    guessesLeft = 9;
+    elGuesses.innerHTMLt = guessesLeft;
+    generateLetter();
+}
